fb1dd79d2494b0a69d2a2f794e695b503fef263b
Sean Kau <sean@kautown.com>
2011-05-09 12:19:15 -0700

    Iinitialized Governess game

---
+++ src/com/kautown/governess/Governess.java
@@ -0,0 +1,19 @@
+package com.kautown.governess;
+
+import jgame.jre.platform.JGEngine;
+
+public class Governess extends JGEngine{
+
+	@Override
+	public void initCanvas() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void initGame() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ src/com/kautown/governess/Main.java
@@ -0,0 +1,7 @@
+package com.kautown.governess;
+
+public class Main {
+	public static void main (String args[]) {
+		System.out.println("Hello World");
+	}
+}
