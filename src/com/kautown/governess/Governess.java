5b1f0e8ddb685151d428c01b5147d75b415dc7f0
unknown <Sean@.(none)>
2011-05-09 18:52:24 -0700

    Claire walks!

--- src/com/kautown/governess/Governess.java
+++ src/com/kautown/governess/Governess.java
@@ -10,7 +10,7 @@ public class Governess extends JGEngine{
 	private static final long serialVersionUID = 1L;
 
 	public static void main (String args[]) {
-		new Governess(640, 480);
+		new Governess(800, 600);
 	}
 	
 	public Governess() {
@@ -19,22 +19,36 @@ public class Governess extends JGEngine{
 	
 	public Governess(int x, int y) {
 		initEngine(x, y);
+		setGameSpeed(1.0);
 	}
 	
 	@Override
 	public void initCanvas() {
-		setCanvasSettings(6, 8, 80, 80, JGColor.white, JGColor.black, new JGFont("Plain", JGFont.PLAIN, 10.0));
+		setCanvasSettings(20, 15, 32, 32, JGColor.white, JGColor.black, new JGFont("Plain", JGFont.PLAIN, 10.0));
 	}
 
 	@Override
 	public void initGame() {
-		defineImage("Claire", "#", 0, "Claire/Walk2.png", "");
-		setFrameRate(60, 10);
-		setGameState("Start");
-		new Player("Claire", true, pfWidth()-50, pfHeight()-20, 42, "Claire");
+		defineImage("CW1R", "p", 0, "Claire/Walk1.png", "");
+		defineImage("CW2R", "p", 0, "Claire/Walk2.png", "");
+		defineImage("CW3R", "p", 0, "Claire/Walk3.png", "");
+		
+		defineImage("CW1L", "p", 0, "Claire/Walk1.png", "x");
+		defineImage("CW2L", "p", 0, "Claire/Walk2.png", "x");
+		defineImage("CW3L", "p", 0, "Claire/Walk3.png", "x");
+		
+		defineAnimation("ClaireR",new String[]{"CW1R", "CW2R", "CW3R", "CW2R"}, 0.2);
+		defineAnimation("ClaireSR",new String[]{"CW2R"}, 0.2);
+		defineAnimation("ClaireSL",new String[]{"CW2R"}, 0.2);
+		defineAnimation("ClaireL",new String[]{"CW1L", "CW2L", "CW3L", "CW2L"}, 0.2);
+		
+		setFrameRate(40, 10);
+		setGameState("InGame");
+		new Player("ClaireS", true, 80, 400, 42, "Claire", this);
 	}
 	
 	public void doFrameInGame() {
 		moveObjects();
 	}
 }
+
--- src/com/kautown/governess/Player.java
+++ src/com/kautown/governess/Player.java
@@ -4,15 +4,39 @@ import jgame.base.JGObject;
 import jgame.jre.platform.JGEngine;
 
 public class Player extends jgame.base.JGObject {	
+	private final double STEP = 1.5;
+	
 	public Player(String name, boolean uniqueId, double xPos, double yPos,
-			int collisionid, String gfxname) {
-		super(name, uniqueId, xPos, yPos, collisionid, gfxname);
+			int collisionid, String gfxname, JGEngine engine) {
+		super("Claire", false, xPos, yPos, 0, "Claire");
+		eng = engine;
 	}
+	
+	private JGEngine eng;
+	private boolean goingRight = true;
+	
 	public void move() {
-		if (eng.getKey(JGEngine.KeyLeft)  && x > 14)              x -= 14*gamespeed;
-		if (eng.getKey(JGEngine.KeyRight) && x < eng.pfWidth()) x += 14*gamespeed;
+		if (eng.getKey(JGEngine.KeyLeft)) {
+			x -= STEP*gamespeed;
+			setAnim("ClaireL");
+			goingRight = false;
+		}
+		else if (eng.getKey(JGEngine.KeyRight)) {
+			x += STEP*gamespeed;
+			setAnim("ClaireR");
+			goingRight = true;
+		}
+		else {
+			if(goingRight) {
+				setAnim("ClaireSR");
+			}
+			else {
+				setAnim("ClaireSL");
+			}
+		}
 		return;
 	}
+	
 	public void hit(JGObject obj) {
 		return;
 	}
